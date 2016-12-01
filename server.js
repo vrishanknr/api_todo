fd82a4de333bf8c5391eaaaadf57e62c420a5859
vrishank nr <vrishanknr@gmail.com>
2016-12-01 12:53:57 -0800

    delete add

--- server.js
+++ server.js
@@ -56,6 +56,20 @@ app.post('/todos', function(req,res) {
 	
 });
 
+app.delete('/todos/:id', function(req,res) {
+	var todoId =parseInt(req.params.id,10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	
+	if (!matchedTodo) {
+		res.status(404).json({"error": "no todo found with that id"});
+	}
+	else{
+		
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
+});
+
 
 
 app.listen(PORT,function () {
