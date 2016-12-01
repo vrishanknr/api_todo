8072cc330de33fb25bc7b1013b3884d049f77c86
vrishank nr <vrishanknr@gmail.com>
2016-12-01 12:35:18 -0800

    refurfished aoi with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express =require('express');
 var bodyParser = require('body-parser');
+var _ =require('underscore');
 
 var app = express();
 var PORT =process.env.PORT || 3000;
@@ -20,12 +21,14 @@ app.get('/todos', function (req,res) {
 
 app.get('/todos/:id', function (req,res){
 	var todoId =parseInt(req.params.id, 10);
-	var matchedTodo;
-	todos.forEach(function(todo) {
-		if (todoId == todo.id){
-			matchedTodo =todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos,{id: todoId});
+	
+	//var matchedTodo;
+	//todos.forEach(function(todo) {
+		//if (todoId == todo.id){
+			//matchedTodo =todo;
+		//}
+	//});
 	if (matchedTodo) {
 		res.json(matchedTodo);
 	}
@@ -39,7 +42,11 @@ app.get('/todos/:id', function (req,res){
 });
 
 app.post('/todos', function(req,res) {
-	var body =req.body;
+	var body =_.pick(req.body, 'description', 'completed');;
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length == 0){
+		return res.status(400).send();
+	}
+	body.description =body.description.trim();
 	
 	body.id=todoNextId+1;
 	todos.push(body);
